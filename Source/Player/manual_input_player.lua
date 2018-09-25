1be5e1fc9346f9f23286a0f921ed84b0a4b982be
Aiku Poker <aikupoker@gmail.com>
2018-09-25 18:18:49 +0200

    Added secure deepstack server and manual player

---
+++ Source/Player/manual_input_player.lua
@@ -0,0 +1,29 @@
+local socket = require("socket")
+local arguments = require 'Settings.arguments'
+
+
+local port = 0
+if #arg > 0 then
+  port = tonumber(arg[1])
+else
+  print("need port")
+  return
+end
+
+connection = assert(socket.connect(arguments.acpc_server, port))
+
+while true do
+
+  print("send message")
+  local msg = io.read()
+
+  connection:send(msg .. '\r\n') 
+
+  local out, status = connection:receive(1)
+
+  print("msg received")
+  print(status)
+
+
+  collectgarbage();collectgarbage()
+end
