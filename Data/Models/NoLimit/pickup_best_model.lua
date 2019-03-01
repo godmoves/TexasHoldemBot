3708342d0aca04a00acf1835de47d93e7aba5f54
godmoves <514158072@qq.com>
2019-03-01 11:11:51 +0800

    fix pick best model

--- Data/Models/NoLimit/pickup_best_model.lua
+++ Data/Models/NoLimit/pickup_best_model.lua
@@ -1,7 +1,9 @@
+--- Pick up the model with lowest validation loss
 require 'torch'
 
 best_loss = 1
 best_epoch = 0
+-- Change the iter number to fit your models
 for i=1,300 do
   table = torch.load('epoch_' .. i .. '_gpu.info')
   if (table.valid_loss < best_loss) then
@@ -11,4 +13,4 @@ for i=1,300 do
 end
 
 print('best epoch: ' .. best_epoch)
-print('bect loss: ' .. best_loss)
+print('best loss: ' .. best_loss)
